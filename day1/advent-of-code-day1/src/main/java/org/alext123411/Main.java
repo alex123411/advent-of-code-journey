9d8d1e965c859d187fad6a234163d739f206c2a7
alex123411 <alextorgovskyh@gmail.com>
2023-06-26 19:25:20 -0500

    Starting advent of code using Java.
    Day 1 task status: Done.

---
+++ day1/advent-of-code-day1/src/main/java/org/alext123411/Main.java
@@ -0,0 +1,33 @@
+package org.alext123411;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Please input file path name where we can find a list of  calories you got from elves.");
+        Scanner filePathInput = new Scanner(System.in);
+        String path = "";
+
+        if (filePathInput.hasNextLine()) path = filePathInput.nextLine();
+
+        Scanner scanner = new Scanner(new File(path));  // Reading file from path
+
+        int temp = 0;
+        int max = 0;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.equals("")) {
+                max = Math.max(temp, max);
+                temp = 0;
+                continue;
+            }
+            temp += Integer.parseInt(line.trim());
+        }
+
+        scanner.close();
+        System.out.println("Biggest number of calories: " + max);  // Output user input
+    }
+}
