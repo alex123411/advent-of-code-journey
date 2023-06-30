59664a24257d11b0f02a79b680596002712832e9
alex123411 <alextorgovskyh@gmail.com>
2023-06-29 21:15:02 -0500

    Day4
    Day 4 advent of code tasks status: Done.

---
+++ day4/src/main/java/org/alex123411/Day4.java
@@ -0,0 +1,83 @@
+package org.alex123411;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Day4 {
+
+    private static final String inputPath = "C:\\Users\\alext\\Documents\\GitHub\\advent-of-code-journey\\day4\\src\\main\\java\\org\\alex123411\\input.txt";
+
+    public void part1() throws FileNotFoundException {
+        List<String> input = readInputFile();
+
+        int res = 0;
+
+        for (String s : input) {
+            String[] sectionsIntervals = s.split(",");
+
+            String[] firstIntervalIndexes = sectionsIntervals[0].split("-");
+            int firstIntervalStart = Integer.parseInt(firstIntervalIndexes[0]);
+            int firstIntervalEnd = Integer.parseInt(firstIntervalIndexes[1]);
+
+            String[] secondIntervalIndexes = sectionsIntervals[1].split("-");
+            int secondIntervalStart = Integer.parseInt(secondIntervalIndexes[0]);
+            int secondIntervalEnd = Integer.parseInt(secondIntervalIndexes[1]);
+
+            if(firstIntervalStart <= secondIntervalStart && firstIntervalEnd >= secondIntervalEnd) res++;
+            else if(firstIntervalStart >= secondIntervalStart && firstIntervalEnd <= secondIntervalEnd) res++;
+        }
+
+        System.out.println("Result: " + res);
+    }
+
+    public void part2() throws FileNotFoundException {
+        List<String> input = readInputFile();
+
+        int res = 0;
+
+        for (String s : input) {
+            String[] sectionsIntervals = s.split(",");
+
+            String[] firstIntervalIndexes = sectionsIntervals[0].split("-");
+            int firstIntervalStart = Integer.parseInt(firstIntervalIndexes[0]);
+            int firstIntervalEnd = Integer.parseInt(firstIntervalIndexes[1]);
+
+            String[] secondIntervalIndexes = sectionsIntervals[1].split("-");
+            int secondIntervalStart = Integer.parseInt(secondIntervalIndexes[0]);
+            int secondIntervalEnd = Integer.parseInt(secondIntervalIndexes[1]);
+
+            if(intervalContainsPoint(firstIntervalStart, secondIntervalStart, secondIntervalEnd)) res++;
+            else if(intervalContainsPoint(firstIntervalEnd, secondIntervalStart, secondIntervalEnd)) res++;
+            else if(intervalContainsPoint(secondIntervalStart, firstIntervalStart, firstIntervalEnd)) res++;
+            else if(intervalContainsPoint(secondIntervalEnd, firstIntervalStart, firstIntervalEnd)) res++;
+
+        }
+
+        System.out.println("Result: " + res);
+    }
+
+    private boolean intervalContainsPoint(int point, int intervalStart, int intervalEnd){
+        return intervalStart <= point && point <= intervalEnd;
+    }
+
+    public int getAlphabeticCounter(char c) {
+        char uppercaseC = Character.toUpperCase(c);
+
+        int alphabeticCounter = uppercaseC - 'A' + 1;
+        return alphabeticCounter + (Character.isUpperCase(c) ? 26 : 0);
+    }
+
+    private List<String> readInputFile() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(inputPath));
+        List<String> input = new ArrayList<>();
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            input.add(line);
+        }
+
+        scanner.close();
+        return input;
+    }
+}
---
+++ day4/src/main/java/org/alex123411/Main.java
@@ -0,0 +1,14 @@
+package org.alex123411;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Day4 day4Tasks = new Day4();
+
+        day4Tasks.part1();
+
+        day4Tasks.part2();
+    }
+}
